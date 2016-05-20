4dc9de190d7c20c9a41213051b19fc22c4157bc4
Mario Koestl <mkoestl@localhost.localdomain>
2016-05-20 11:45:28 +0200

    latest changes of the applied prgoramming lecture

--- perl/appliedSave/mkoestl_Ex05.pl
+++ perl/appliedSave/mkoestl_Ex05.pl
@@ -14,20 +14,27 @@ use Bioinformatics::Restriction;
 
 my $usage= << "JUS";
   Title:    mkoestl_Ex05
-  usage:    mkoestl_Ex05 -file [inputFile]
-  
-  options: -file|f  [REQUIRED]   Input fasta file
+  usage:    mkoestl_Ex05 -file [inputFile] 
+  options: -file|f  [REQUIRED] [string]  : Input DNA sequence in fasta format
+           -enzym|e  [OPTIONAL] [string] : Enzym cut pattern in regex format, default = "[ag]gatc[ct]"
+           -circular|c [OPTIONAL] [string] : TRUE or FALSE if dna seq is circular, defualt = FALSE
            -help|h               print this message     
 
-  results: calculate fragments sizes after restriction enzm has cutted given dna sequence
+  results: calculate fragments sizes after restriction enzym has cutted given dna sequence
 JUS
 
 ########c#########
 my $opt_file;
 my $opt_help;
+my $opt_res="[ag]gatc[ct]";
+my $opt_circ="FALSE";
+
 GetOptions(
   "h|help|" => \$opt_help,
-  "file|f=s" => \$opt_file);
+  "file|f=s" => \$opt_file,
+  "enzym|e=s" => \$opt_res,
+  "circular|c=s" => \$opt_circ);
+
 
 #print usage if requested
 if($opt_help){
@@ -41,18 +48,17 @@ if (! -f $opt_file) {
 }
 
 my $dna_seq = @{Bioinformatics::Pattern::getFastaSeqs($opt_file)}[0];
-my $resPattern="[ag]gatc[ct]";
 my @pos_Array = Bioinformatics::Pattern::getPatternMatchPositions(
                     "sequence"=> $dna_seq,
-                    "pattern"=> $resPattern,
-                    "is_circular"=>"TRUE");
+                    "pattern"=> $opt_res,
+                    "is_circular"=>$opt_circ);
 
 my @frags = Bioinformatics::Restriction::getFragmentLengths(
             "pos_array"=> \@pos_Array,
             "seq_Length"=>length($dna_seq),
-            "is_circular"=>"TRUE");
+            "is_circular"=>$opt_circ);
 
-print "If sequence : \n\n $dna_seq \n\n is cutted with restriction enzymPatter $resPattern following gragment lengths appear: \n\n"; 
+print "If sequence : \n\n $dna_seq \n\n with is_circular = $opt_circ is cutted with restriction enzym Pattern $opt_res, therefore following fragment lengths appear: \n\n"; 
 foreach (@frags)
 {
     print "$_\n";
